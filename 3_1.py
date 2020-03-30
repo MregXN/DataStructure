53107c7b0ea2430fe7112999456f890f2f21d1da
MregXn <mregxn@gmail.com>
2020-03-30 16:01:33 +0800

    3.1

--- 3_1.py
+++ 3_1.py
@@ -8,7 +8,7 @@
 #   ps: 同构指两棵子树T1和T2，如果T1可以通过若干次左右孩子互换变成T2，则我们称两棵树是同构的
 #
 #   利用结构数组表示二叉树： 静态链表，左右儿子用数组下标表示，不存在用-表示
-# 
+#
 import math as m
 import numpy as np
 NULL = "-"
@@ -23,9 +23,9 @@ class Node(object):
 
 class Tree(object):
     def __init__(self, input):
-        self.tree = [None]*input[0]
+        self.tree = [None] * input[0]
         node = Node()
-        for i in np.arange(1, len(input), 1): 
+        for i in np.arange(1, len(input), 1):
             pos = m.floor((i - 1) / 3)
             remainder = (i - 1) % 3
 
@@ -41,26 +41,54 @@ class Tree(object):
     def PrintAll(self):
         count = 0
         for i in self.tree:
-            count += 1
             print("element" + str(count))
             print(i.element)
             print("left")
             print(i.left)
             print("right")
             print(i.right)
+            count += 1
 
     def FindRoot(self):
-        check = [0]*len(self.tree)
+        check = [0] * len(self.tree)
         for i in self.tree:
             if i.left != NULL:
-                check[i.left] =1;
+                check[i.left] = 1
             if i.right != NULL:
-                check[i.right] =1;           
+                check[i.right] = 1
 
-        for i in np.arange(0, len(check), 1): 
+        for i in np.arange(0, len(check), 1):
             if check[i] != 1:
-                self.Root = self.tree[i].element
+                self.Root = i
+                return self.Root
+
+        self.Root = NULL
         return self.Root
+
+
+def Isomorphic(T1, T2, r1, r2):
+
+    if (r1 == NULL) and (r2 == NULL):
+        return 1
+
+    if ((r1 == NULL) and (r2 != NULL)) or ((r1 != NULL) and (r2 == NULL)):
+        return 0
+
+    if (T1.tree[r1].element != T2.tree[r2].element):
+        return 0
+
+    if (T1.tree[r1].left == NULL) and (T2.tree[r2].left == NULL):
+        return Isomorphic(T1, T2, T1.tree[r1].right, T1.tree[r2].right)
+
+    if ((T1.tree[r1].left != NULL) and
+        (T2.tree[r2].left != NULL)) and (T1.tree[T1.tree[r1].left].element ==
+                                         T2.tree[T2.tree[r2].left].element):
+        return (Isomorphic(T1, T2, T1.tree[r1].left, T2.tree[r2].left)
+                and Isomorphic(T1, T2, T1.tree[r1].right, T2.tree[r2].right))
+    else:
+        return (Isomorphic(T1, T2, T1.tree[r1].left, T2.tree[r2].right)
+                and Isomorphic(T1, T2, T1.tree[r1].left, T2.tree[r2].right))
+
 
 if __name__ == "__main__":
     input1 = [
@@ -74,5 +102,4 @@ if __name__ == "__main__":
 
     T1 = Tree(input1)
     T2 = Tree(input2)
-    print(T1.FindRoot())
-    print(T2.FindRoot())
+    print(Isomorphic(T1, T2, T1.FindRoot(), T2.FindRoot()))
